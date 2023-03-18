f474f3a3acb96da413c64be1e1630f3f5e05577f
Nora <nsorym04@gmail.com>
2023-03-18 18:59:16 +0900

    otherlink grid

--- wp-content/themes/JUDY-theme/front-page.php
+++ wp-content/themes/JUDY-theme/front-page.php
@@ -1,2 +1,9 @@
 <?php get_header(); ?>
+<section class="otherlink wrapper">
+    <div class="otherlink_company">
+
+    </div>
+    <div class="otherlink_welfare"></div>
+    <div class="otherlink_message"></div>
+</section>
 <?php get_footer(); ?>
