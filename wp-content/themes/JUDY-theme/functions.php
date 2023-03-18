3e45f29750cf135a7767ba1d23a972a65aa7d7ba
Nora <nsorym04@gmail.com>
2023-03-18 12:52:34 +0900

    カスタム投稿タイプを追加

--- wp-content/themes/JUDY-theme/functions.php
+++ wp-content/themes/JUDY-theme/functions.php
@@ -11,4 +11,34 @@ function load_my_styles()
 }
 add_action('wp_enqueue_scripts', 'load_my_styles');
 
+//カスタム投稿タイプ
+function create_post_type()
+{
+    register_post_type('requirements', [
+        'labels' => [
+            'name' => '募集要項',
+            'singular_name' => 'requirements',
+        ],
+        'public' => true,
+        'has_archive' => true,
+        'menu_position' => 5,
+        'show_in_rest' => true,
+        'supports' => array('title', 'editor', 'thumbnail'),
+    ]);
+    register_taxonomy(
+        'requirements-category',
+        'requirements',
+        array(
+            'hierarchical' => true,
+            'update_count_callback' => '_update_post_term_count',
+            'label' => '募集要項カテゴリー',
+            'singular_label' => '募集要項カテゴリー',
+            'public' => true,
+            'show_ui' => true,
+        )
+    );
+}
+add_action('init', 'create_post_type');
+
+
 add_theme_support('post-thumbnails');
--- wp-content/themes/JUDY-theme/header.php
+++ wp-content/themes/JUDY-theme/header.php
@@ -10,3 +10,13 @@
 </head>
 
 <body>
+    <header class="header">
+        <nav class="header_nav inner">
+            <ul class="header_nav_list">
+                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
+                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
+                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
+                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
+            </ul>
+        </nav>
+    </header>
