e09dfd72d03eee4fe92a4286823d84b52c443e23
Nora <nsorym04@gmail.com>
2023-03-18 16:09:10 +0900

    header完成

--- wp-content/themes/JUDY-theme/functions.php
+++ wp-content/themes/JUDY-theme/functions.php
@@ -2,11 +2,12 @@
 //スタイル、スクリプトの読み込み
 function load_my_styles()
 {
-    wp_enqueue_style('style', get_template_directory_uri() . 'src/css/style.css');
+    wp_enqueue_style('style', get_template_directory_uri() . '/src/css/style.css');
     wp_enqueue_style('reset', "//unpkg.com/ress/dist/ress.min.css", array('style'));
+    wp_enqueue_style('google-font', '//fonts.googleapis.com/css?family=Yatra+One|Zen+Antique+Soft&display=swap');
 
     wp_enqueue_script('jquery');
-    wp_enqueue_script('script', get_template_directory_uri() . 'src/js/index.js', 'jquery');
+    wp_enqueue_script('script', get_template_directory_uri() . '/src/js/index.js', 'jquery');
     wp_enqueue_script('inview', '//cdnjs.cloudflare.com/ajax/libs/protonet-jquery.inview/1.1.2/jquery.inview.min.js', array('jquery'));
 }
 add_action('wp_enqueue_scripts', 'load_my_styles');
@@ -32,7 +33,7 @@ function create_post_type()
             'hierarchical' => true,
             'update_count_callback' => '_update_post_term_count',
             'label' => '募集要項カテゴリー',
-            'singular_label' => '募集要項カテゴリー',
+            'singular_label' => 'requirements_category',
             'public' => true,
             'show_ui' => true,
         )
--- wp-content/themes/JUDY-theme/header.php
+++ wp-content/themes/JUDY-theme/header.php
@@ -13,10 +13,11 @@
     <header class="header">
         <nav class="header_nav inner">
             <ul class="header_nav_list">
-                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
-                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
-                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
-                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<span>Company</span></li>
+                <li><a href="<?php echo esc_url(home_url("/company/")); ?>"></a>会社概要<br><span>Company</span></li>
+                <li><a href="<?php echo esc_url(home_url("/requirements/")); ?>"></a>募集要項<br><span>Requirements</span></li>
+                <li><a href="<?php echo esc_url(home_url("/welfare/")); ?>"></a>数字で見る<br><span>Welfare</span></li>
+                <li><a href="<?php echo esc_url(home_url("/entry/")); ?>"></a>エントリー<br><span>Entry</span></li>
             </ul>
+            <div class="header_nav_img"><a href="<?php echo esc_url(home_url()); ?>"></a><img src="<?php echo esc_url(get_theme_file_uri('img/logo.png')); ?>" alt=""></div>
         </nav>
     </header>
