0fbab994f7eb0063526c7931969878ca9e7bf2a0
Nora <nsorym04@gmail.com>
2023-03-18 21:31:14 +0900

    添える手完成

--- wp-content/themes/JUDY-theme/front-page.php
+++ wp-content/themes/JUDY-theme/front-page.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
 <section class="otherlink wrapper">
     <div class="otherlink_company">
-
+        <div class="inner otherlink_company_inner">
+            <div class="otherlink_company_in flex_center_column">
+                <div class="rotate flex_column">
+                    <h1>「添える手」</h1>
+                    <p class="rotate_txt">お食事やお飲み物をはじめ、お客様が触れるものは多く存在します。<br>添えるという心構えが丁寧な接客に繋がります</p>
+                </div>
+            </div>
+            <button class="link_btn">
+                会社について
+                <a href=""></a>
+            </button>
+        </div>
     </div>
     <div class="otherlink_welfare"></div>
     <div class="otherlink_message"></div>
