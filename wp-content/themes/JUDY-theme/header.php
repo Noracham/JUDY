33c1ecb7607a3594ff8edff4725d3ca4748d7232
Nora <nsorym04@gmail.com>
2023-03-18 18:32:45 +0900

    hero完成

--- wp-content/themes/JUDY-theme/header.php
+++ wp-content/themes/JUDY-theme/header.php
@@ -21,3 +21,23 @@
             <div class="header_nav_img"><a href="<?php echo esc_url(home_url()); ?>"></a><img src="<?php echo esc_url(get_theme_file_uri('img/logo.png')); ?>" alt=""></div>
         </nav>
     </header>
+    <section class="hero inner">
+        <div class="hero_txt">
+            <?php if (is_front_page() || is_home()) :; ?>
+                <p class="hero_txt_title">添える手に<br>愛と喜びを持って</p>
+            <?php else :; ?>
+                <?php
+                if (is_page()) {
+                    $title = get_the_title();
+                    $enTitle = get_post_field('post_name', get_the_ID());
+                } elseif (get_post_type() === 'requirements') {
+                    $title = get_post_type_object(get_post_type())->label;
+                    $enTitle = get_post_type_object(get_post_type())->name;
+                }; ?>
+                <p class="hero_txt_subtitle"><?php echo $title; ?></p>
+                <p class="hero_txt_entitle">
+                    <?php echo $enTitle; ?>
+                </p>
+            <?php endif; ?>
+        </div>
+    </section>
