7e7bf72e72e0f3b54df62f16581e8ef1d2d66d7c
Nora <nsorym04@gmail.com>
2023-03-20 12:16:23 +0900

    company_about完成

---
+++ wp-content/themes/JUDY-theme/page-company.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+<section class="company_about wrapper">
+    <div class="company_about_one">
+        <div class="inner">
+            <div class="company_about_one_in flex_center">
+                <div class="rotate flex_column">
+                    <h1>「添える手」</h1>
+                    <p class="rotate_txt">お食事やお飲み物をはじめ、お客様が触れるものは多く存在します<br><br>添えるという心構えが丁寧な接客に繋がります</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="company_about_one">
+        <div class="inner">
+            <div class="company_about_one_in flex_center">
+                <div class="rotate flex_column">
+                    <h1>「喜びと愛」</h1>
+                    <p class="rotate_txt">調理から接客までお客様から食材にまで喜びと愛を持って<br><br>何事にも感謝を忘れない<br>そんな料理店です</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<?php get_footer(); ?>
