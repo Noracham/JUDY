2ee453ec4ab664d4bd7cafc1b78112374df97807
Nora <nsorym04@gmail.com>
2023-03-23 12:34:17 +0900

    welfare仮

---
+++ wp-content/themes/JUDY-theme/page-welfare.php
@@ -0,0 +1,51 @@
+<?php get_header(); ?>
+<section class="welfare wrapper">
+    <div class="welfare_content inner">
+        <div class="welfare_content_one bwframe">
+            <div class="welfare_content_one_title">
+                <div class="welfare_content_one_title_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/welfare_home.png')); ?>" alt="">
+                </div>
+                <h1>お休み</h1>
+            </div>
+            <p class="flex_center">年間<span>１２０</span>日以上</p>
+        </div>
+        <div class="welfare_content_one bwframe">
+            <div class="welfare_content_one_title">
+                <div class="welfare_content_one_title_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/welfare_home.png')); ?>" alt="">
+                </div>
+                <h1>お休み</h1>
+            </div>
+            <p class="flex_center">年間<span>１２０</span>日以上</p>
+        </div>
+        <div class="welfare_content_one bwframe">
+            <div class="welfare_content_one_title">
+                <div class="welfare_content_one_title_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/welfare_home.png')); ?>" alt="">
+                </div>
+                <h1>お休み</h1>
+            </div>
+            <p class="flex_center">年間<span>１２０</span>日以上</p>
+        </div>
+        <div class="welfare_content_one bwframe">
+            <div class="welfare_content_one_title">
+                <div class="welfare_content_one_title_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/welfare_home.png')); ?>" alt="">
+                </div>
+                <h1>お休み</h1>
+            </div>
+            <p class="flex_center">年間<span>１２０</span>日以上</p>
+        </div>
+        <div class="welfare_content_one bwframe">
+            <div class="welfare_content_one_title">
+                <div class="welfare_content_one_title_img">
+                    <img src="<?php echo esc_url(get_theme_file_uri('img/welfare_home.png')); ?>" alt="">
+                </div>
+                <h1>お休み</h1>
+            </div>
+            <p class="flex_center">年間<span>１２０</span>日以上</p>
+        </div>
+    </div>
+</section>
+<?php get_footer(); ?>
