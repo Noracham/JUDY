95b88b083bad99bf25c0c7f5a87ab2f74e6d730e
Nora <nsorym04@gmail.com>
2023-03-23 10:49:17 +0900

    個別募集要項、全体的に手直し

--- wp-content/themes/JUDY-theme/single-requirements.php
+++ wp-content/themes/JUDY-theme/single-requirements.php
@@ -2,12 +2,13 @@
 <!-- <div class="test"></div> -->
 <section class="requirements_single">
     <div class="requirements_single_content inner black_frame">
-        <div class="black flex_center">キッチンスタッフ
+        <div class="black flex_center">
             <?php
+            $job = get_field('requirements1')['value'];
             $term = get_the_terms($post->ID, 'requirements-category');
             if ($term && !is_wp_error($term)) {
                 $first_term = reset($term);
-                echo '【' . $first_term->name . '】';
+                echo $job . '【' . $first_term->name . '】';
             }
             ?></div>
         <ul class="requirements_single_content_list">
