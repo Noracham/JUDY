9950a3322bf0ee7f98bb6b4bc86b515b67219135
Nora <nsorym04@gmail.com>
2023-03-23 10:29:55 +0900

    募集要項個別ページ完成

--- wp-content/themes/JUDY-theme/single-requirements.php
+++ wp-content/themes/JUDY-theme/single-requirements.php
@@ -2,7 +2,16 @@
 <!-- <div class="test"></div> -->
 <section class="requirements_single">
     <div class="requirements_single_content inner black_frame">
+        <ul class="requirements_single_content_list">
+            <?php
+            for ($i = 1; $i < 99999; $i++) {
+                $requirements = get_field('requirements' . $i);
+                if ($requirements == null) break;
 
+                echo '<li><p class="title">' . $requirements['title'] . '</p><p class="value">' . $requirements['value'] . '</p></li>';
+            }
+            ?>
+        </ul>
     </div>
 </section>
 <?php get_footer(); ?>
