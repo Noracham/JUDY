c339a159dcae56200abaf1adfe14f7601c70faa1
Nora <nsorym04@gmail.com>
2023-03-23 10:43:36 +0900

    募集要項の黒帯

--- wp-content/themes/JUDY-theme/single-requirements.php
+++ wp-content/themes/JUDY-theme/single-requirements.php
@@ -2,6 +2,14 @@
 <!-- <div class="test"></div> -->
 <section class="requirements_single">
     <div class="requirements_single_content inner black_frame">
+        <div class="black flex_center">キッチンスタッフ
+            <?php
+            $term = get_the_terms($post->ID, 'requirements-category');
+            if ($term && !is_wp_error($term)) {
+                $first_term = reset($term);
+                echo '【' . $first_term->name . '】';
+            }
+            ?></div>
         <ul class="requirements_single_content_list">
             <?php
             for ($i = 1; $i < 99999; $i++) {
