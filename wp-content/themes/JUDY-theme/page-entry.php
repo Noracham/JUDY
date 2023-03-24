d590ca59b151d35221eca4a7b7d0c84d39a78599
Nora <nsorym04@gmail.com>
2023-03-24 12:11:28 +0900

    最後のバリデーション位置とスタイル調整

--- wp-content/themes/JUDY-theme/page-entry.php
+++ wp-content/themes/JUDY-theme/page-entry.php
@@ -1,3 +1,9 @@
 <?php get_header(); ?>
-    <?php the_content(); ?>
+<section class="entry wrapper">
+    <div class="entry_content black_frame">
+        <div class="entry_content_form">
+            <?php the_content(); ?>
+        </div>
+    </div>
+</section>
 <?php get_footer(); ?>
